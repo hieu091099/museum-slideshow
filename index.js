04a8ac077bcf4da6feeb7a6c419051f260d2a8ba
hieu091099 <vai22806@gmail.com>
2023-06-20 14:47:59 +0700

    init

---
+++ index.js
@@ -0,0 +1,413 @@
+const data = [
+  {
+    title: "戰國中期 嵌綠松石金屬絲犧尊",
+    url: "https://sketchfab.com/models/0bc78a5e4ff94bc4bb3bebd46985be2d/embed?autostart=1&ui_vr=0&ui_settings=0&ui_infos=0&ui_inspector=0&ui_stop=0&ui_watermark=1&ui_watermark_link=1",
+    information: {
+      統一編號: "故銅002382",
+      品名: "嵌綠松石金屬絲犧尊",
+      朝代: "戰國中期",
+      尺寸: "長39.5公分；寬14.5公分；高29.0公分",
+      類別: "銅器",
+      材質: "銅",
+      古物分級: "重要古物",
+    },
+    introduction: {
+      簡介: "全器作四蹄動物穩立狀，立耳，圓眼，四蹄足，尾下垂。以寫實的風格呈現動物姿態及肌理。做為戰國時期的酒器，以獸口為流，背上開口，有活頁式蓋。 器表為黑褐色，間以青綠。通體以銀絲鑲嵌斜角雲紋，並飾以華美的綠松石及金銀鑲嵌。在面部，以圓金鑲嵌為眼珠，鼻梁、眉、額以綠松石點綴。頸部嵌金一周，表示項圈。背蓋上為蟠龍形金銀鑲嵌。",
+    },
+    previewImage: "./images/17009601.jpg",
+    secondTab: ["17009601.jpg", "17009874.jpg", "17009875.jpg"],
+  },
+  {
+    title: "清 乾隆 掐絲琺瑯鳧尊",
+    url: "https://sketchfab.com/models/3960ead633c4424498efe8d19f39057f/embed?autostart=1&ui_vr=0&ui_settings=0&ui_infos=0&ui_inspector=0&ui_stop=0&ui_watermark=1&ui_watermark_link=1",
+    information: {
+      統一編號: "故琺000096",
+      品名: "掐絲琺瑯鳧尊",
+      朝代: "清 乾隆",
+      尺寸: "高11.3公分；長16.7公分；寬6.0公分；口徑4.2公分",
+      類別: "珍玩",
+      材質: "銅",
+    },
+    introduction: {
+      簡介: "掐絲琺瑯鳧尊，造型如鴨，長頸短喙，腿短有蹼，尾部羽毛展開上揚如扇形，鴨身加一瓶口，上有提梁，造型忠實遵循《西清古鑑》卷九「周鳧尊二」，但比例與輪廓更富拙趣。器身前後的獸面紋飾和雙翅羽毛絢麗的色彩，寓巧於拙，古雅不失莊重，表現出與銅器金銀錯迴然不同的美感，充分說明乾隆朝宮廷的藝術品味。",
+    },
+    previewImage: "./images/17011026.jpg",
+    secondTab: ["17011026.jpg", "17011027.jpg", "17011028.jpg", "17011029.jpg"],
+  },
+  {
+    title: "清 乾隆 霽青描金游魚轉心瓶",
+    url: "https://sketchfab.com/models/07cbc7e7228a48dd8a8622b70e072ca1/embed?autostart=1&ui_vr=0&ui_settings=0&ui_infos=0&ui_inspector=0&ui_stop=0&ui_watermark=1&ui_watermark_link=1",
+    information: {
+      統一編號: "故瓷017646",
+      品名: "霽青描金游魚轉心瓶",
+      朝代: "清 乾隆",
+      尺寸: "高23.5公分；深19.8公分；口徑5.3公分；足徑8.4公分",
+      類別: "陶瓷",
+      材質: "瓷器",
+      古物分級: "國寶",
+    },
+    introduction: {
+      簡介: "套瓶，斂口捲邊，長頸，豐肩，斂腹，矮圈足，器肩飾有四圓環形繫耳。瓶腹分內外兩層，內層施淺青湖綠色釉表現出湖水一樣的背景，其間以粉彩畫水草、落花及金魚。器外底施湖綠釉，書有「大清乾隆年製」青花篆體款識。外瓶器腹部上作四鏤空開光，觀者可自頸部握住轉動，透過外瓶開光看見內瓶所繪之水草魚紋悠游嬉戲於眼前，如同走馬燈一般，故名「轉心瓶」。",
+    },
+    previewImage: "./images/17010581.jpg",
+    secondTab: [
+      "17010581.jpg",
+      "17010582.jpg",
+      "17010583.jpg",
+      "17010584.jpg",
+      "17010822.jpg",
+      "17010823.jpg",
+    ],
+  },
+  {
+    title: "清 乾隆 清 十八世紀 乾隆御用款 金碗",
+    url: "https://sketchfab.com/models/0c5b303b6474451381f3ce4ec3cbd0e9/embed?autostart=1&ui_vr=0&ui_settings=0&ui_infos=0&ui_inspector=0&ui_stop=0&ui_watermark=1&ui_watermark_link=1",
+    information: {
+      統一編號: "故雜001241",
+      品名: "清 十八世紀 乾隆御用款 金碗",
+      朝代: "清 乾隆",
+      尺寸: "高7.3公分；徑14.7公分",
+      類別: "珍玩",
+      材質: "金",
+    },
+    introduction: {
+      簡介: "金碗貯放於木胎描金圓皮盒中，盒蓋內白綾上書寫漢、滿、蒙、藏四種文字記載金碗的來歷：「乾隆四十九（1784）年十二月十七日哈密札薩克郡王品級貝勒額爾德希爾呈進金碗一件。」據考證，札薩克郡王六世額爾德希爾為答謝乾隆皇帝下詔其家族世襲爵位，因此進呈此金碗。這件來自新疆的金碗，以掐絲技法在口沿與圈足裝飾花葉紋，碗腹鏨刻石榴並掐絲花葉紋，再利用極具特色的黃金焊細珠工藝，將極細小金珠裝飾鋪地，使碗壁佈滿金珠，雖如同鏨刻魚子紋，卻呈現出特別的質感與異域風格，圈足內陰刻雙鉤「乾隆御用」楷款。是為乾隆皇帝御用器。",
+    },
+    previewImage: "./images/b_24011119.jpg",
+    secondTab: ["17011043.jpg", "17011044.jpg", "17011045.jpg"],
+  },
+  {
+    title: "清 肉形石",
+    url: "https://sketchfab.com/models/55ceb40b452542c78fd03ceec0c6bc99/embed?autostart=1&ui_vr=0&ui_settings=0&ui_infos=0&ui_inspector=0&ui_stop=0&ui_watermark=1&ui_watermark_link=1",
+    information: {
+      統一編號: "故雜000178",
+      品名: "肉形石",
+      朝代: "清",
+      尺寸: "全高6.6公分；長7.9公分",
+      類別: "珍玩",
+      材質: "玉石",
+      古物分級: "重要古物",
+    },
+    introduction: {
+      簡介: "似玉美石中，石英類的瑪瑙、碧石等，亦因為具有特殊且多樣的紋理與色彩，而經常施以巧雕。「肉形石」即碧石類礦物，其紋理層層堆疊，藝匠據此天然特徵再予以加工。首先於表面細密鑽點，除營造毛孔的效果外，還使質地較為酥鬆，易於染色。其次便是將上層染成褐紅色，像是肉皮浸過醬油一般。",
+    },
+    previewImage: "./images/17009644.jpg",
+    secondTab: ["17009644.jpg", "17010820.jpg"],
+  },
+  {
+    title: "明 永樂 青花穿蓮龍紋天球瓶",
+    url: "https://sketchfab.com/models/336b6222a5be4588b8fe6a1b14e66314/embed?autostart=1&ui_vr=0&ui_settings=0&ui_infos=0&ui_inspector=0&ui_stop=0&ui_watermark=1&ui_watermark_link=1",
+    information: {
+      統一編號: "故瓷011423",
+      品名: "青花穿蓮龍紋天球瓶",
+      朝代: "明 永樂",
+      尺寸: "高42.9公分；口徑9.7公分；足徑15.8公分",
+      類別: "陶瓷",
+      材質: "瓷器",
+    },
+    introduction: {
+      簡介: "青花天球瓶碩大渾厚，器身環繞威武雄猛的穿蓮三爪行龍，整體氣勢堂皇莊重，是國立故宮博物院所藏永樂青花瓷器中的名品。天球瓶的胎體緻密，釉面瑩潤。口略外侈、直頸、圓鼓腹、平底微凹、無圈足。通體繪飾一回首張望的穿蓮三爪行龍，張口吐舌，露牙雙目圓睜，闊步張爪，龍身滿畫鱗甲，雄武矯健。瓶頸及背景空隙處則繪轉枝蓮花紋飾，青花艷麗濃重，繪畫線條流暢，展現此時製瓷的高超工藝。 永樂、宣德年間，時有青花大型器如天球瓶、扁壺等作品的燒製。青花瓷器是以含氧化鈷的礦物為顏料，在瓷器坯體上描繪紋飾，施一層透明釉後，經約攝氏一千兩百度左右高溫燒造而成的作品。明代永樂、宣德時期的青花發色濃豔，並且帶有滲青鐵鏽斑點，形成獨特鮮明的風格，且成為清朝早期製作永樂宣德青花仿古瓷時的模仿重點。",
+    },
+    previewImage: "./images/17010985.jpg",
+    secondTab: ["17010985.jpg"],
+  },
+];
+
+let htmlString = "";
+let thumbnailString = "";
+
+for (let index in data) {
+  const information = data[index].information;
+  const informationKeys = Object.keys(information);
+  const introduction = data[index].introduction;
+  const introductionKeys = Object.keys(introduction);
+
+  let informationString = "";
+  informationKeys.forEach((key) => {
+    const value = information[key];
+    informationString += `<dl><dt>${key}</dt><dd>${value}</dd></dl>`;
+  });
+  let carouselSecondTabString = "";
+  let thumbnailStringChildren = "";
+  for (let image of data[index].secondTab) {
+    carouselSecondTabString += `<li class="splide__slide slide-child"><img class="image-child" src="./images/${
+      parseInt(index) + 1
+    }/${image}"/></li>`;
+  }
+
+  for (let i in data[index].secondTab) {
+    thumbnailStringChildren += `<li class="thumb-li thumbnail-${index}">
+          <img style="width:100px;height:100px"
+          src="./images/${parseInt(index) + 1}/${data[index].secondTab[i]}"
+          alt=""
+          />
+          </li>`;
+  }
+
+  htmlString += `  <li class="splide__slide" >
+  <h1>戰國中期 嵌綠松石金屬絲犧尊</h1>
+  <div class="flex">
+    <div class="col">
+        <div class="tab">
+            <button class="tablinks active" tab-id="3D${index}" onclick="activeTab(event, '3D${index}')">3D</button>
+            <button class="tablinks" tab-id="2D${index}" onclick="activeTab(event, '2D${index}')">2D</button>
+        </div>
+        <div id="3D${index}" class="tabcontent active">
+            <iframe
+            style="width: 100%; height: 70vh"
+            src="${data[index].url}"
+            frameborder="0"
+         ></iframe>
+        </div>
+        <div id="2D${index}" class="tabcontent">
+            <section id="slide-${
+              parseInt(index) + 1
+            }" class="splide splide-child another-slide${index}" aria-label="Splide Basic HTML Example">
+                <div class="splide__track">
+                    <ul class="splide__list">
+                       ${carouselSecondTabString}
+                    </ul>
+                </div>
+            </section>
+            <ul id="thumbnails-${index}" class="thumbs thumbnails-${index}">
+              ${thumbnailStringChildren}
+            </ul>
+        </div>
+    </div>
+    <div class="col">
+      <div>
+       ${informationString}
+      </div>
+      <div class="article">
+        <h2 class="article-title">${introductionKeys[0]}</h2>
+        <p>
+          ${introduction[introductionKeys[0]]}
+        </p>
+      </div>
+    </div>
+  </div>
+</li>`;
+
+  thumbnailString += `<li class="thumbnail">
+          <img
+          src="${data[index].previewImage}"
+          alt=""
+          />
+          </li>`;
+  //   thumbnailString += `<li class="thumbnail">
+  //       <iframe
+
+  //       src="${data[item].url}"
+  //       frameborder="0"
+  //     ></iframe>
+  //         </li>`;
+}
+
+let content = document.querySelector("#content");
+let thumbnail = document.querySelector("#thumbnails");
+content.innerHTML = htmlString;
+thumbnail.innerHTML = thumbnailString;
+
+/** MAIN SLIDE */
+let splide = new Splide("#main-slider", {
+  width: "95%",
+  height: "80vh",
+  interval: 5000,
+  perPage: 1,
+  autoplay: true,
+  pagination: false,
+  arrows: false,
+});
+
+let thumbnails = document.getElementsByClassName("thumbnail");
+let current;
+
+splide.on("mounted move", function () {
+  let thumbnail = thumbnails[splide.index];
+  if (thumbnail) {
+    if (current) {
+      current.classList.remove("is-active");
+    }
+
+    thumbnail.classList.add("is-active");
+    current = thumbnail;
+  }
+});
+
+/** MOUNT CHILDRENT SLIDE */
+
+// for (let i = 2; i <= data.length; i++) {
+//   new Splide(`#slide-${i}`, {
+//     arrows: false,
+//     type: "loop",
+//     interval: 1000,
+//     pagination: false,
+//     autoplay: true,
+//   }).mount();
+// }
+
+let splide1 = new Splide("#slide-1", {
+  type: "loop",
+  autoplay: true,
+  interval: 1000,
+  pagination: false,
+  arrows: false,
+});
+
+let thumbnails1 = document.getElementsByClassName("thumbnail-0");
+let current1;
+
+splide1.on(" move", function () {
+  let thumbnail = thumbnails1[splide1.index];
+
+  if (thumbnail) {
+    if (current1) {
+      current1.classList.remove("is-active");
+    }
+    thumbnail.classList.add("is-active");
+    current1 = thumbnail;
+  }
+});
+splide1.mount();
+
+let splide2 = new Splide("#slide-2", {
+  type: "loop",
+  autoplay: true,
+  interval: 1000,
+  pagination: false,
+  arrows: false,
+});
+
+let thumbnails2 = document.getElementsByClassName("thumbnail-1");
+let current2;
+
+splide2.on(" move", function () {
+  let thumbnail = thumbnails2[splide2.index];
+
+  if (thumbnail) {
+    if (current2) {
+      current2.classList.remove("is-active");
+    }
+    thumbnail.classList.add("is-active");
+    current2 = thumbnail;
+  }
+});
+splide2.mount();
+
+let splide3 = new Splide("#slide-3", {
+  type: "loop",
+  autoplay: true,
+  interval: 1000,
+  pagination: false,
+  arrows: false,
+});
+
+let thumbnails3 = document.getElementsByClassName("thumbnail-2");
+let current3;
+
+splide3.on(" move", function () {
+  let thumbnail = thumbnails3[splide3.index];
+
+  if (thumbnail) {
+    if (current3) {
+      current3.classList.remove("is-active");
+    }
+    thumbnail.classList.add("is-active");
+    current3 = thumbnail;
+  }
+});
+splide3.mount();
+
+let splide4 = new Splide("#slide-4", {
+  type: "loop",
+  autoplay: true,
+  interval: 1000,
+  pagination: false,
+  arrows: false,
+});
+
+let thumbnails4 = document.getElementsByClassName("thumbnail-3");
+let current4;
+
+splide4.on(" move", function () {
+  let thumbnail = thumbnails4[splide4.index];
+
+  if (thumbnail) {
+    if (current4) {
+      current4.classList.remove("is-active");
+    }
+    thumbnail.classList.add("is-active");
+    current4 = thumbnail;
+  }
+});
+splide4.mount();
+
+let splide5 = new Splide("#slide-5", {
+  type: "loop",
+  autoplay: true,
+  interval: 1000,
+  pagination: false,
+  arrows: false,
+});
+
+let thumbnails5 = document.getElementsByClassName("thumbnail-4");
+let current5;
+
+splide5.on(" move", function () {
+  let thumbnail = thumbnails5[splide5.index];
+
+  if (thumbnail) {
+    if (current5) {
+      current5.classList.remove("is-active");
+    }
+    thumbnail.classList.add("is-active");
+    current5 = thumbnail;
+  }
+});
+splide5.mount();
+
+let splide6 = new Splide("#slide-6", {
+  type: "loop",
+  autoplay: true,
+  interval: 1000,
+  pagination: false,
+  arrows: false,
+});
+
+let thumbnails6 = document.getElementsByClassName("thumbnail-5");
+let current6;
+
+splide6.on(" move", function () {
+  let thumbnail = thumbnails6[splide6.index];
+
+  if (thumbnail) {
+    if (current6) {
+      current6.classList.remove("is-active");
+    }
+    thumbnail.classList.add("is-active");
+    current6 = thumbnail;
+  }
+});
+splide6.mount();
+
+splide.mount();
+
+function activeTab(evt, tabId) {
+  let i, tablinks;
+
+  let twoDemensionTab = document.getElementById(`2D${tabId.substr(-1)}`);
+  let threeDemensionTab = document.getElementById(`3D${tabId.substr(-1)}`);
+
+  twoDemensionTab.classList.remove("active");
+  threeDemensionTab.classList.remove("active");
+
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    if (tablinks[i].getAttribute("tab-id").substr(-1) === tabId.substr(-1)) {
+      tablinks[i].className = tablinks[i].className.replace("active", "");
+    }
+  }
+
+  document.getElementById(tabId).classList.add("active");
+  evt.currentTarget.className += " active";
+}
